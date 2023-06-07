bf6b567fd731828970b758a3c2be388b6de7bffe
bengomar <omar.bengorine@orange.fr>
2023-06-07 13:31:01 +0100

    slides Modal

--- js.js
+++ js.js
@@ -2,7 +2,8 @@ const site = "http://localhost:8000/api/v1/titles/";
 let imdb_score = site+"?sort_by=-votes,-imdb_score&page_size=7"; //Les 7 meilleurs films
 let urlGenre = site+"?sort_by=-votes,-imdb_score&page_size=7&genre="; // Les 7 meilleurs films par genre
 
-
+// fonction permettant la récupération de données depuis l'API.
+// La fonction accepte un paramètre, une url.
 function fetchingApiViaUrl(url) {
     return fetch(url)
     .then((response) => {
@@ -10,51 +11,64 @@ function fetchingApiViaUrl(url) {
     })
 }
 
+// fonction permettant le déplacement des images des carrousels.
+// Cela à l'aide des 2 flèches aux extrémités de chaque carrousels.
 function slideArrowsFunction(genre, left_arrow, right_arrow) {
 
+// flèche de gauche
     for (var i = 0; i < left_arrow.length; i++) {
         let arrow_prev = left_arrow[i]
         arrow_prev.addEventListener("click", () => {
               genre.scrollBy(-180, 0);
         })
     }
+// flèche de droite
     for (var i = 0; i < right_arrow.length; i++) {
         let arrow_next = right_arrow[i]
         arrow_next.addEventListener("click", () => {
             genre.scrollBy(+180, 0);
         })
     }
-
 }
 
-function ModalMovieInformations(filmInfo) {
-    let modalImage = document.getElementById("modalImage");
-    let modalTitle = document.getElementById("modalTitle");
-    let modalGenres = document.getElementById("modalGenres");
-    let modalYear = document.getElementById("modalYear");
-    let modalRated = document.getElementById("modalRated");
-    let modalImdb_score = document.getElementById("modalImdb_score");
-    let modalRelease = document.getElementById("modalRelease");
-    let modalActors = document.getElementById("modalActors");
-    let modalDuration = document.getElementById("modalDuration");
-    let modalCountries = document.getElementById("modalCountries");
-    let modalWorldwide_gross_income = document.getElementById("modalWorldwide_gross_income");
-    let modalResume = document.getElementById("modalResume");
+// fonction permettant l'affichage des détails d'un film dans une modal.
+function ModalMovieInformations(movie_id) {
+   url = site+movie_id
+   console.log(url)
+   fetchingApiViaUrl(url)
+   .then(data => {
+        let modalImage = document.getElementById("modalImage");
+        let modalTitle = document.getElementById("modalTitle");
+        let modalGenres = document.getElementById("modalGenres");
+        let modalYear = document.getElementById("modalYear");
+        let modalRated = document.getElementById("modalRated");
+        let modalImdb_score = document.getElementById("modalImdb_score");
+        let modalRelease = document.getElementById("modalRelease");
+        let modalActors = document.getElementById("modalActors");
+        let modalDuration = document.getElementById("modalDuration");
+        let modalCountries = document.getElementById("modalCountries");
+        let modalWorldwide_gross_income = document.getElementById("modalWorldwide_gross_income");
+        let modalResume = document.getElementById("modalResume");
 
-    modalImage.style.backgroundImage = `url('${filmInfo.image_url}')`;
-    modalTitle.innerText = "Titre: " + filmInfo.title;
-    modalGenres.innerText = "Genre: " + filmInfo.genres[0];
-    modalYear.innerText = "Année: " + filmInfo.year;
-    modalRated.innerText = "Rated: " + filmInfo.rated;
-    modalImdb_score.innerText = "Score Imdb: " + filmInfo.imdb_score;
-    modalRelease.innerText = "Réalisateurs: " + filmInfo.directors;
-    modalActors.innerText = "Acteurs: " + filmInfo.actors;
-    modalDuration.innerText = "Durée: " + filmInfo.duration + " min.";
-    modalCountries.innerText = "Pays: " + filmInfo.countries;
-    modalWorldwide_gross_income.innerText = "Résultat au Box Office: " + filmInfo.worldwide_gross_income;
-    modalResume.innerText = "Résumé: \n" + filmInfo.long_description;
+        modalImage.style.backgroundImage = `url('${data.image_url}')`;
+        modalTitle.innerText = "Titre: " + data.title;
+        modalGenres.innerText = "Genre: " + data.genres[0];
+        modalYear.innerText = "Année: " + data.year;
+        modalRated.innerText = "Rated: " + data.rated;
+        modalImdb_score.innerText = "Score Imdb: " + data.imdb_score;
+        modalRelease.innerText = "Réalisateurs: " + data.directors;
+        modalActors.innerText = "Acteurs: " + data.actors;
+        modalDuration.innerText = "Durée: " + data.duration + " min.";
+        modalCountries.innerText = "Pays: " + data.countries;
+        modalWorldwide_gross_income.innerText = "Résultat au Box Office: " + data.worldwide_gross_income;
+        modalResume.innerText = "Résumé: \n" + data.long_description;
+        });
+   ModalTriggering("myModal", "myBtn")
+   //console.log(id_film);
+   //ModalTriggering("myModal", "id_film")
 }
 
+// Fonction de création et gestion de la modal.
 function ModalTriggering(modalId, triggerId){
 
     // Get the modal
@@ -97,11 +111,10 @@ fetchingApiViaUrl(imdb_score)
 
     best_image.style.backgroundImage = `url('${best_film.image_url}')`;
     best_title.innerText = best_film.title;
+    url_film_id = site+best_film.id
 
-    return fetch(`http://localhost:8000/api/v1/titles/${best_film.id}`);
-})
-.then((response) => {
-    return response.json();
+    return fetchingApiViaUrl(url_film_id);
+    //return fetch(`http://localhost:8000/api/v1/titles/${best_film.id}`);
 })
 .then((filmInfo) => {
     //console.log(filmInfo);
@@ -109,53 +122,62 @@ fetchingApiViaUrl(imdb_score)
     let best_description = document.getElementById("bestFilm_resume");
     best_description.innerText = filmInfo.description;
 
-    ModalMovieInformations(filmInfo)
+    ModalMovieInformations(filmInfo.id)
 })
 
 
 // the best films imdb ranking
-
 fetchingApiViaUrl(imdb_score)
 .then((data) => {
 
-    let movies = data;
     let best_movies_rank = document.getElementById("rank");
-    let cpt = 1;
-    let listPics = [];
     let left_arrow = document.getElementsByClassName("rank-arrow-left")
     let right_arrow = document.getElementsByClassName("rank-arrow-right");
     slideArrowsFunction(best_movies_rank, left_arrow, right_arrow);
     best_movies_rank.scrollLeft = 0
+
+    let movies = data;
+    let cpt = 1;
+    //let listPics = [];
 
     movies.results.forEach(function(movie){
         const movie_div = document.createElement("div");
         movie_div.classList.add("movie-cover");
         movie_div_id = 'rank-img' + cpt;
         movie_div.setAttribute("id", movie_div_id);
-        cpt++;
         movie_div.style.backgroundImage = `url('${movie.image_url}')`;
-        listPics.push(movie_div_id);
+        movie_div.setAttribute("onclick", `ModalMovieInformations(${movie.id})`);
+        //movie_div.onclick = ModalMovieInformations(movie.id, movie_div_id);
+        cpt++;
+        //listPics.push(movie_div_id);
         best_movies_rank.appendChild(movie_div);
-        console.log(movie.id);
-        return fetch(`http://localhost:8000/api/v1/titles/${movie.id}`);
+
+
+//        fetchingApiViaUrl(site+movie.id)
+//                .then((filmInfo) => {
+//                console.log(filmInfo);
+//                })
     })
-    
+    //console.log(movie_div_id);
+    //ModalTriggering("myModal", movie_div_id);
 
 
-/*
 
-    let ImagesRank = document.getElementsByClassName("movie-cover");
-    for (var i = 0; i < ImagesRank.length; i++) {
-           let ClickImgId = ImagesRank[i];
-               ClickImgId.addEventListener("click", () => {
-                   alert(ClickImgId.id);
-               })
-           }
+//    listPics.forEach(pic => console.log(pic)
+//    );
+//    console.log(movie.id)
+//    let ImagesRank = document.getElementsByClassName("movie-cover");
+//    for (var i = 0; i < ImagesRank.length; i++) {
+//           let ClickImgId = ImagesRank[i];
+//               ClickImgId.addEventListener("click", () => {
+//                   alert(ClickImgId.id);
+//               })
+//           }
 
     //listPics.forEach(function(item, array) {
     //    console.log(item);
     //})
-*/
+
 })
 
 
@@ -244,5 +266,7 @@ fetchingApiViaUrl(urlGenre+'Biography')
 */
 
 // Modal + infos du Film le mieux noté.
+//ModalTriggering("myModal", "myBtn")
 
-ModalTriggering("myModal", "myBtn")
+
+
